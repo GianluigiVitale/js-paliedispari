e9d78b6a10d1ef216ad9904a513759c2d59ba5cb
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-02-24 17:50:51 +0100

    es1 with function

--- es1 parola palindroma/js/script.js
+++ es1 parola palindroma/js/script.js
@@ -5,14 +5,19 @@ Scopo del gioco: Una funzione per capire se la parola è palindroma
 */
 
 //
-var parola = prompt('Inserisci una parola per scoprire se e\' palindroma')  // prompt per chiedere parola
-var parolaReversed = '';                                                    // definisco una variabile per la parola al contrario
+var parola = prompt('Inserisci una parola per scoprire se e\' palindroma');  // prompt per chiedere parola
 
-for (var i = parola.length - 1; i >= 0; i--) {                             // ciclo for che si ripete tante volte quante sono i caratteri di 'parola', ad ogni ciclo prende l'ultimo carattere di 'parola' e lo aggiunge alla stringa 'parolaReversed'
-    parolaReversed += parola[i];
+var parolaVerifica = revert(parola);
+
+function revert(parola) {
+    var parolaReversed = '';
+    for (var i = parola.length - 1; i >= 0; i--) {                   // ciclo for che si ripete tante volte quante sono i caratteri di 'parola', ad ogni ciclo prende l'ultimo carattere di 'parola' e lo aggiunge alla stringa 'parolaReversed'
+        parolaReversed += parola[i];
+    }
+    return parolaReversed
 }
 
-if (parola == parolaReversed) {                                             // se 'parola' e' uguale a 'parolaReversed' vuol dire che la parola e' palindroma
+if (parola == parolaVerifica) {                                      // se 'parola' e' uguale a 'parolaReversed' vuol dire che la parola e' palindroma
     console.log('La parola e\' polindroma!');
 } else {
     console.log('La parola non e\' polindroma!');
